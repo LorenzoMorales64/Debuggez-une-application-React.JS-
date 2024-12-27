1a99cb5ad3a0be6a3ac5a778056840400eef7734
LorenzoMorales <mrllorenzo@hotmail.com>
2024-12-27 16:40:19 +0100

    Commit du Slider modifié

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  
+  const byDateDesc = data?.focus
+    ? data.focus.sort(
+        (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date) // Tri par date décroissante
+      )
+    : [];
+  const nextCard = () => {
+    setIndex((prevIndex) =>
+      prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+  );
+  };
+  useEffect(() => {
+    const intervalId = setInterval(nextCard, 5000);
+    return () => clearInterval(intervalId);
+  }, [index, byDateDesc]);
+
+  return (
+    <div className="SlideCardList">
+      {byDateDesc.length > 0 ? (
+        byDateDesc.map((event, idx) => (
+          <div
+            key={event.id || idx} // Utiliser event.id pour la clé unique ou idx en secours
+            className={`SlideCard SlideCard--${
+              index === idx ? 'display' : 'hide'
+            }`}
+          >
+            <img src={event.cover} alt={event.title} />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>Aucun événement à afficher</div> // Message alternatif si aucun événement
+      )}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((event, radioIdx) => (
+            <input
+              key={event.id || radioIdx} // Utiliser event.id pour la clé unique ou radioIdx en secours
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx} // Vérifie si l'index correspond au bouton radio
+              onChange={() => setIndex(radioIdx)} // Change l'index en cliquant sur le bouton radio
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
